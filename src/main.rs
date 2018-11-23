1a7bcdec797838368fdbbc9d7d45038c57f6d757
Bas Zalmstra <zalmstra.bas@gmail.com>
2018-11-23 20:16:18 +0100

    removed unused stuff

--- src/main.rs
+++ src/main.rs
@@ -24,52 +24,6 @@ fn main() {
     let context = glutin::ContextBuilder::new().with_vsync(true);
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    let vertex_buffer = {
-        #[derive(Copy, Clone)]
-        struct Vertex {
-            position: [f32; 3],
-        }
-
-        implement_vertex!(Vertex, position);
-
-        let vertex1 = Vertex { position: [-0.5, 0.0, 0.0] };
-        let vertex2 = Vertex { position: [0.0, 0.5, 0.0] };
-        let vertex3 = Vertex { position: [0.5, 0.0, 0.0] };
-        let shape = vec![vertex1, vertex2, vertex3];
-
-        glium::VertexBuffer::new(&display, &shape).unwrap()
-    };
-
-
-    let program = {
-        let vertex_shader_src = r#"
-            #version 140
-
-            in vec3 position;
-
-            uniform mat4 viewProjection;
-            uniform mat4 model;
-
-            void main() {
-                gl_Position = viewProjection*(model*vec4(position, 1.0));
-            }
-        "#;
-
-        let fragment_shader_src = r#"
-            #version 140
-
-            out vec4 color;
-
-            void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        "#;
-
-        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap()
-    };
-
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
     let mut camera = Camera::new();
     camera.translate_by(&Vector3::new(0.0, 0.0, -2.0));
 
@@ -116,8 +70,6 @@ fn main() {
                 nalgebra::Matrix4::new_translation(&nalgebra::Vector3::new(0.0, 0.0, -2.0))*
                 nalgebra::Matrix4::from_euler_angles(0.0,rotation,0.0))
         };
-
-        //frame.draw(&vertex_buffer, &indices, &program, &triangle_uniforms, &Default::default()).unwrap();
 
         planet_renderer.draw(&mut frame, &frustum, &planet_transform);
 
