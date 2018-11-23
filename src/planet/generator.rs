54364963646c0a708588bcfec432c8fd27ee043f
Sjoerd van den Dries <svddries@smart-robotics.nl>
2018-11-23 22:32:36 +0100

    fix: fixed normals

--- src/planet/generator.rs
+++ src/planet/generator.rs
@@ -1,6 +1,6 @@
 use super::constants::{NORMALS_PER_PATCH, VERTICES_PER_PATCH};
 use crate::planet;
-use nalgebra::{Point3, Vector3};
+use nalgebra::{Point3, Vector2, Vector3};
 use planet::geometry_provider::{PatchGeometry, PatchLocation};
 use planet::GeometryProvider;
 use std::mem;
@@ -17,24 +17,51 @@ impl Generator {
     pub fn description(&self) -> &planet::Description {
         &self.description
     }
+
+    fn compute_vertex(&self, x: f64, y: f64, patch: &PatchLocation) -> Point3<f64> {
+        let oriented_position = patch.face.orientation() * Vector3::new(x, y, 1.0);
+        let dir = morph(oriented_position);
+
+        let height = 0.05 * f64::sin(30.0 * (dir.x + dir.y + dir.z));
+
+        (dir * (self.description.radius + height)).into()
+    }
+
+    fn compute_normal(&self, x: f64, y: f64, patch: &PatchLocation) -> Vector3<f64> {
+        let eps = 0.001;
+
+        let px1 = self.compute_vertex(x - eps, y, patch);
+        let px2 = self.compute_vertex(x + eps, y, patch);
+        let py1 = self.compute_vertex(x, y - eps, patch);
+        let py2 = self.compute_vertex(x, y + eps, patch);
+
+        let x_diff = px2 - px1;
+        let y_diff = py2 - py1;
+
+        x_diff.cross(&y_diff).normalize()
+    }
 }
 
 impl GeometryProvider for Generator {
     fn provide(&self, patch: PatchLocation) -> PatchGeometry {
-        let step = patch.size/(VERTICES_PER_PATCH as f64-1.0);
-        let mut positions:Vec<Point3<f64>> = Vec::with_capacity(VERTICES_PER_PATCH*VERTICES_PER_PATCH);
-        let mut normals:Vec<Vector3<f64>> = Vec::with_capacity(NORMALS_PER_PATCH*NORMALS_PER_PATCH);
 
+        // Generate vertices
+        let vertex_step = patch.size/(VERTICES_PER_PATCH as f64-1.0);
+        let mut positions:Vec<Point3<f64>> = Vec::with_capacity(VERTICES_PER_PATCH*VERTICES_PER_PATCH);
         for y in 0..VERTICES_PER_PATCH {
             for x in 0..VERTICES_PER_PATCH {
-                let local_position = Vector3::<f64>::new(x as f64 * step - 0.5,y as f64 * step - 0.5,0.5) * 2.0;
-                let oriented_position = patch.face.orientation() * local_position;
-                let dir = morph(oriented_position);
+                let local_position = Vector2::<f64>::new(x as f64 * vertex_step - 0.5,y as f64 * vertex_step - 0.5) * 2.0;
+                positions.push(self.compute_vertex(local_position.x, local_position.y, &patch));
+            }
+        };
 
-                let height = 0.05 * f64::sin(30.0 * (dir.x + dir.y + dir.z));
-
-                positions.push((dir * (self.description.radius + height)).into());
-                normals.push(dir);
+        // Generate normals
+        let normal_step = patch.size/(NORMALS_PER_PATCH as f64-1.0);
+        let mut normals:Vec<Vector3<f64>> = Vec::with_capacity(NORMALS_PER_PATCH*NORMALS_PER_PATCH);
+        for y in 0..NORMALS_PER_PATCH {
+            for x in 0..NORMALS_PER_PATCH {
+                let local_position = Vector2::<f64>::new(x as f64 * normal_step - 0.5,y as f64 * normal_step - 0.5) * 2.0;
+                normals.push(self.compute_normal(local_position.x, local_position.y, &patch));
             }
         };
 
@@ -43,6 +70,7 @@ impl GeometryProvider for Generator {
             normals
         }
     }
+
 }
 
 fn morph(pos: Vector3<f64>) -> Vector3<f64> {
@@ -53,3 +81,4 @@ fn morph(pos: Vector3<f64>) -> Vector3<f64> {
                 pos.y * f64::sqrt(1.0 - a.y - b.y + pos_squared.z*pos_squared.x/3.0),
                 pos.z * f64::sqrt(1.0 - a.z - b.z + pos_squared.x*pos_squared.y/3.0))
 }
+
